e2df0c07e69918dd3d4422b9d89548474c87b98f
owncreed93 <christiantarifer@gmail.com>
2020-05-12 13:49:10 -0500

    formulario, view y template editar categoria

--- app/inv/views.py
+++ app/inv/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
-from .models import Categoria
 
+from .models import Categoria
+from .forms import CategoriaForm
 
 # Create your views here.
 class CategoriaView(LoginRequiredMixin, generic.ListView):
@@ -11,3 +13,30 @@ class CategoriaView(LoginRequiredMixin, generic.ListView):
     template_name = 'inv/categoria_list.html'
     context_object_name = "obj"
     login_url = 'bases:login'
+
+
+class CategoriaNew(LoginRequiredMixin, generic.CreateView):
+    model = Categoria
+    template_name = 'inv/categoria_form.html'
+    context_object_name = 'obj'
+    form_class = CategoriaForm
+    success_url = reverse_lazy('inv:categoria_list')
+    login_url = 'bases:login'
+
+    def form_valid(self, form):
+        form.instance.uc = self.request.user
+
+        return super().form_valid(form)
+    
+class CategoriaEdit(LoginRequiredMixin, generic.UpdateView):
+    model = Categoria
+    template_name = 'inv/categoria_form.html'
+    context_object_name = 'obj'
+    form_class = CategoriaForm
+    success_url = reverse_lazy('inv:categoria_list')
+    login_url = 'bases:login'
+
+    def form_valid(self, form):
+        form.instance.um = self.request.user.id
+
+        return super().form_valid(form)
