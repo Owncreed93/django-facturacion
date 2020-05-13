4c1908f626b7e1cee595079a15e7e7d271e90ce1
owncreed93 <christiantarifer@gmail.com>
2020-05-13 18:56:39 -0500

    creacion de modelo subcategoria

--- app/inv/models.py
+++ app/inv/models.py
@@ -21,5 +21,23 @@ class Categoria(ClaseModelo):
         self.descripcion = self.descripcion.upper()
         super(Categoria, self).save()
 
+
+class Subcategoria(ClaseModelo):
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    descripcion = models.CharField(
+        max_length=100, 
+        help_text="Descripción de la subcategoría",
+    )
+
+    class Meta:
+        verbose_name_plural = "Subcategorías"
+        unique_together = ('categoria', 'descripcion')
+
+    def __str__(self):
+        return f'{self.categoria.descripcion} : {self.descripcion}'
+
+    def save(self):
+        self.descripcion = self.descripcion.upper()
+        super(Subcategoria, self).save()
     
 
