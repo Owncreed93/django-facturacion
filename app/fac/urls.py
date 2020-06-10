4ed1843a27db4316a563f9ed3b57f3dcd64a5187
owncreed93 <christiantarifer@gmail.com>
2020-06-10 16:34:39 -0500

    Agregada función de activar o inactivar clientes

--- app/fac/urls.py
+++ app/fac/urls.py
@@ -4,11 +4,13 @@ from .views import (
     ClienteView,
     ClienteNew,
     ClienteEdit,
+    clienteInactivar,
 )
 
 urlpatterns = [
     # * CLIENTES
     path('clientes/', ClienteView.as_view(), name='cliente_list'),
     path('clientes/new', ClienteNew.as_view(), name='cliente_new'),
-    path('clientes/edit/<int:pk>', ClienteEdit.as_view(), name='cliente_edit')
+    path('clientes/edit/<int:pk>', ClienteEdit.as_view(), name='cliente_edit'),
+    path('clientes/estado/<int:id>', clienteInactivar, name='cliente_inactivar'),
 ]
--- app/fac/views.py
+++ app/fac/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.messages.views import SuccessMessageMixin
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
@@ -48,3 +50,17 @@ class ClienteEdit(VistaBaseEdit):
     form_class = ClienteForm
     success_url = reverse_lazy('fac:cliente_list')
     permission_required = 'fac.change_cliente'
+
+@login_required(login_url="/login/")
+@permission_required("fac.change_cliente", login_url='/login/')
+def clienteInactivar(request, id):
+    cliente = Cliente.objects.filter(pk=id).first()
+
+    if request.method == 'POST':
+        if cliente:
+            cliente.estado = not cliente.estado
+            cliente.save()
+            return HttpResponse('OK')
+    return HttpResponse('FAIL')   
+    
+    return HttpResponse('FAIL')
