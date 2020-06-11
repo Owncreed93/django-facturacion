0c77f3f7148368a368e2e0c9b90e64b671b60355
owncreed93 <christiantarifer@gmail.com>
2020-06-10 21:17:00 -0500

    Refactorizado views create y update de modelo Clientes

--- app/fac/views.py
+++ app/fac/views.py
@@ -3,38 +3,29 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views import generic
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+)
 
 
-from bases.views import SinPrivilegios
-
-
+from bases.views import (
+    SinPrivilegios,
+    VistaBaseCreate,
+    VistaBaseEdit,
+)
 from .models import Cliente
 from .forms import ClienteForm
 
+
 # Create your views here.
-class ClienteView(SinPrivilegios, generic.ListView):
+class ClienteView(SinPrivilegios, ListView):
     permission_required = 'cmp.view_cliente'
     model = Cliente
     template_name = 'cmp/cliente_list.html'
     context_object_name = 'obj'
 
-
-class VistaBaseCreate(SuccessMessageMixin, SinPrivilegios, generic.CreateView):
-    context_object_name = 'obj'
-    success_message = 'Cliente agregado satisfactoriamente'
-
-    def form_valid(self, form):
-        form.instance.uc = self.request.user
-        return super().form_valid(form)
-
-class VistaBaseEdit(SuccessMessageMixin, SinPrivilegios, generic.UpdateView):
-    context_object_name = 'obj'
-    success_message = 'Cliente actualizado satisfactoriamente'
-
-    def form_valid(self, form):
-        form.instance.um = self.request.user.id
-        return super().form_valid(form)
 
 class ClienteNew(VistaBaseCreate):
     model=Cliente
