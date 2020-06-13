298f9175fa1e9e8aa160a4a362512a2ef7715032
owncreed93 <christiantarifer@gmail.com>
2020-06-13 14:19:51 -0500

    Creado modelos clasemodelo2 con django userforeign key, modelos cliente, facturaenc y facturadet para modulo factura

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'inv',
     'cmp',
     'fac',
+    'django_userforeignkey',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_userforeignkey.middleware.UserForeignKeyMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
